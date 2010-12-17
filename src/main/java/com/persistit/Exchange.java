ac0d5602d1b56905473b1f37f48e596449927381
Peter Beaman <pbeaman@akiban.com>
2010-12-17 18:42:04 -0500

    Add public constructor new Exchange(Tree)

--- src/main/java/com/persistit/Exchange.java
+++ src/main/java/com/persistit/Exchange.java
@@ -232,8 +232,8 @@ public class Exchange implements BuildConstants {
      *            name does not already exist.
      * @throws PersistitException
      */
-    public Exchange(Persistit persistit, Volume volume, String treeName, boolean create)
-            throws PersistitException {
+    public Exchange(Persistit persistit, Volume volume, String treeName,
+            boolean create) throws PersistitException {
         _persistit = persistit;
         _key = new Key(persistit);
         _spareKey1 = new Key(persistit);
@@ -268,18 +268,18 @@ public class Exchange implements BuildConstants {
     }
 
     /**
-     * Package-private constructor used only to access the directory Tree for a
-     * Volume.
+     * Construct a new <tt>Exchange</tt> to access the specified {@link Tree}.
      * 
      * @param tree
+     *            The <tt>Tree</tt> to access.
      */
-    Exchange(Persistit persistit, Tree tree) {
-        _persistit = persistit;
-        _key = new Key(persistit);
-        _spareKey1 = new Key(persistit);
-        _spareKey2 = new Key(persistit);
-        _value = new Value(persistit);
-        _spareValue = new Value(persistit);
+    public Exchange(Tree tree) {
+        _persistit = tree._persistit;
+        _key = new Key(_persistit);
+        _spareKey1 = new Key(_persistit);
+        _spareKey2 = new Key(_persistit);
+        _value = new Value(_persistit);
+        _spareValue = new Value(_persistit);
         _volume = tree.getVolume();
         _tree = tree;
         _pool = _persistit.getBufferPool(_volume.getPageSize());
@@ -301,7 +301,11 @@ public class Exchange implements BuildConstants {
         if (tree == null) {
             throw new TreeNotFoundException(treeName);
         }
+        init(tree);
+    }
 
+    void init(final Tree tree) {
+        final Volume volume = tree.getVolume();
         _pool = _persistit.getBufferPool(volume.getPageSize());
         _transaction = _persistit.getTransaction();
         _timeout = _persistit.getDefaultTimeout();
