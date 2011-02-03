179ed027d62e1df4640ce149d81a11fee478a2de
Padraig O'Sullivan <osullivan.padraig@gmail.com>
2011-02-03 11:14:30 -0500

    Removing some un-needed text.

--- core/src/main/java/com/persistit/AntiValue.java
+++ core/src/main/java/com/persistit/AntiValue.java
@@ -17,25 +17,6 @@ package com.persistit;
 
 import com.persistit.exception.InvalidKeyException;
 
-/*
- * Copyright (c) 2005 Persistit Corporation. All Rights Reserved.
- *
- * The Java source code is the confidential and proprietary information
- * of Persistit Corporation ("Confidential Information"). You shall
- * not disclose such Confidential Information and shall use it only in
- * accordance with the terms of the license agreement you entered into
- * with Persistit Corporation.
- *
- * PERSISTIT CORPORATION MAKES NO REPRESENTATIONS OR WARRANTIES ABOUT THE
- * SUITABILITY OF THE SOFTWARE, EITHER EXPRESS OR IMPLIED, INCLUDING BUT
- * NOT LIMITED TO THE IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR
- * A PARTICULAR PURPOSE, OR NON-INFRINGEMENT. PERSISTIT CORPORATION SHALL
- * NOT BE LIABLE FOR ANY DAMAGES SUFFERED BY LICENSEE AS A RESULT OF USING,
- * MODIFYING OR DISTRIBUTING THIS SOFTWARE OR ITS DERIVATIVES.
- * 
- * Created on Jul 14, 2005
- */
-
 /**
  * Represents the end of a key range to be removed. Used in Transactions.
  * 
--- core/src/test/java/com/persistit/test/TestRunner.java
+++ core/src/test/java/com/persistit/test/TestRunner.java
@@ -13,23 +13,6 @@
  * along with this program.  If not, see http://www.gnu.org/licenses.
  */
 
-/*
- *
- * The Java source code is the confidential and proprietary information
- * of Persistit Corporation ("Confidential Information"). You shall
- * not disclose such Confidential Information and shall use it only in
- * accordance with the terms of the license agreement you entered into
- * with Persistit Corporation.
- *
- * PERSISTIT CORPORATION MAKES NO REPRESENTATIONS OR WARRANTIES ABOUT THE
- * SUITABILITY OF THE SOFTWARE, EITHER EXPRESS OR IMPLIED, INCLUDING BUT
- * NOT LIMITED TO THE IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR
- * A PARTICULAR PURPOSE, OR NON-INFRINGEMENT. PERSISTIT CORPORATION SHALL
- * NOT BE LIABLE FOR ANY DAMAGES SUFFERED BY LICENSEE AS A RESULT OF USING,
- * MODIFYING OR DISTRIBUTING THIS SOFTWARE OR ITS DERIVATIVES.
- * 
- * Created on Apr 12, 2004
- */
 package com.persistit.test;
 
 import java.io.BufferedReader;
