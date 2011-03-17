5a11fce67d772779b3853dc92df0f4d51f86ba75
Peter Beaman <pbeaman@akiban.com>
2011-03-17 14:34:08 -0400

    Add some managment tools.  Faster TXN commit through TransactionBuffer.

---
+++ core/src/main/java/com/persistit/TransactionWriter.java
@@ -0,0 +1,39 @@
+/**
+ * Copyright (C) 2011 Akiban Technologies Inc.
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License, version 3,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see http://www.gnu.org/licenses.
+ */
+
+package com.persistit;
+
+import com.persistit.exception.PersistitIOException;
+
+public interface TransactionWriter {
+    
+    boolean writeTransactionStartToJournal(final long startTimestamp)
+            throws PersistitIOException;
+
+    boolean writeTransactionCommitToJournal(final long timestamp,
+            final long commitTimestamp) throws PersistitIOException;
+    
+    boolean writeStoreRecordToJournal(final long timestamp,
+            final int treeHandle, final Key key, final Value value)
+            throws PersistitIOException;
+    
+    boolean writeDeleteRecordToJournal(final long timestamp,
+            final int treeHandle, final Key key1, final Key key2)
+            throws PersistitIOException;
+    
+    boolean writeDeleteTreeToJournal(final long timestamp,
+            final int treeHandle) throws PersistitIOException;
+    
+}
