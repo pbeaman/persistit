3f7c593a9e7e4c76b985ecf8c3f65e767a729832
Nathan Williams <nwilliams@akiban.com>
2011-12-07 11:14:38 -0500

    Add javadoc to new prune methods, consolidate some helpers

--- core/src/main/java/com/persistit/Exchange.java
+++ core/src/main/java/com/persistit/Exchange.java
@@ -647,12 +647,12 @@ public class Exchange {
             _toRemove.add(new OffsetAndLength(offset, length));
         }
 
-        int getRemainingCount() {
-            return _versionCount - _removeCount;
+        boolean noVersionsLeft() {
+            return (_versionCount - _removeCount) == 0;
         }
-
-        long getMaxVersion() {
-            return _maxVersion;
+        
+        boolean isPrimordial() {
+            return (_versionCount - _removeCount) == 1 && _maxVersion != MVV.VERSION_NOT_FOUND;
         }
 
         int getMaxOffset() {
@@ -689,6 +689,12 @@ public class Exchange {
          * AntiValues
          **/
         public static final int DONT_JOURNAL = 1 << 5;
+    }
+
+    static enum PruneStatus {
+        REMOVED,
+        CHANGED,
+        UNCHANGED
     }
 
     /**
@@ -3834,9 +3840,12 @@ public class Exchange {
     }
 
     /**
-     * Temporary(?) location of prune method. Fully prunes the exchanges
-     * current key.
-     * @throws PersistitException for any error
+     * Reduce this Exchanges current <code>Key</code> and <code>Value</code> pair
+     * to the minimal possible state. Does not explicitly modify any other value
+     * in the associated page.
+     *
+     * @throws PersistitException
+     *            For any internal error.
      */
     void prune() throws PersistitException {
         _persistit.checkClosed();
@@ -3850,21 +3859,11 @@ public class Exchange {
 
             if ((foundAt & EXACT_MASK) != 0) {
                 buffer.fetch(foundAt, _spareValue);
-                if (isLongRecord(_spareValue)) {
-                    throw new UnsupportedOperationException("Cannot prune LONG_RECORD");
+                PruneStatus status = prune(_spareValue);
+                if (handlePruneStatus(status, _spareValue)) {
+                    boolean splitRequired = putLevel(lc, _key, _spareValue, buffer, foundAt, false);
+                    Debug.$assert0.t(!splitRequired);
                 }
-
-                boolean keepKey = prune(_spareValue);
-                if (!keepKey) {
-                    // TODO: Extract 'quickDelete' code from raw_removeInternal
-                    //       If quick is not possible, add to pruner thread
-                    // Simple for now: turn into anti-value so it won't be seen
-                    _spareValue.clear().putAntiValueMVV();
-                }
-
-                boolean splitRequired = putLevel(lc, _key, _spareValue, buffer, foundAt, false);
-                Debug.$assert0.t(!splitRequired);
-                
                 _volume.getStatistics().bumpFetchCounter();
                 _tree.getStatistics().bumpFetchCounter();
             }
@@ -3872,20 +3871,34 @@ public class Exchange {
             if (buffer != null) {
                 buffer.releaseTouched();
             }
-            _treeHolder.verifyReleased();
         }
     }
 
-    private void prune(Buffer buffer, Key key, Value value) throws PersistitException {
+    /**
+     * Visit all keys in the buffer and prune each value. After returning, all
+     * values in the buffer will be in most primordial state possible.
+     *
+     * @param buffer
+     *            Buffer to prune. Write claim must be taken by callee.
+     * @param extraKey
+     *            Key used in the process of traversing the buffer.
+     * @param extraValue
+     *            Spare value used during prune process, essentially scratch space.
+     * @throws PersistitException
+     *            For any internal error.
+     */
+    private void prune(Buffer buffer, Key extraKey, Value extraValue) throws PersistitException {
         Debug.$assert0.t(buffer.isWriter());
-        key.clear();
-        int foundAt = buffer.nextKey(key, buffer.getKeyBlockStart() | EXACT_MASK);
+        extraKey.clear();
+        int foundAt = buffer.nextKey(extraKey, buffer.getKeyBlockStart() | EXACT_MASK);
         while (!buffer.isAfterRightEdge(foundAt)) {
-            buffer.fetch(foundAt, value);
-            prune(value);
-            int success = buffer.putValue(key, value, foundAt, false);
-            Debug.$assert0.t(success > 0);
-            foundAt = buffer.nextKey(key, foundAt);
+            buffer.fetch(foundAt, extraValue);
+            PruneStatus status = prune(extraValue);
+            if (handlePruneStatus(status, extraValue)) {
+                int success = buffer.putValue(extraKey, extraValue, foundAt, false);
+                Debug.$assert0.t(success > 0);
+            }
+            foundAt = buffer.nextKey(extraKey, foundAt);
         }
         long timestamp = timestamp();
         buffer.writePageOnCheckpoint(timestamp);
@@ -3893,40 +3906,59 @@ public class Exchange {
     }
 
     /**
+     * @param status
+     *            Status from last prune operation.
      * @param value
-     *            Value to modify in-place by pruning away all unneeded versions
-     * @return <code>true</code> if the key associated should be kept
-     * @throws PersistitException For any error
+     *            Value that was just pruned.
+     * @return <code>true</code> if the value was changed in some way.
      */
-    private boolean prune(Value value) throws PersistitException {
+    private boolean handlePruneStatus(PruneStatus status, Value value) {
+        if (status == PruneStatus.UNCHANGED) {
+            return false;
+        }
+        if (status == PruneStatus.REMOVED) { 
+            // TODO: Extract 'quickDelete' code from raw_removeInternal
+            // or add to prune thread. Simple for now: turn into AntiValue
+            value.clear().putAntiValueMVV();
+        }
+        return true;
+    }
+
+    /**
+     * Reduce the given value to the most primordial state possible. The value
+     * will be modified in-place.
+     *
+     * @param value
+     *            Value to remove all obsolete versions from.
+     * @return Status of the prune operation.
+     * @throws PersistitException
+     *            For any error
+     */
+    private PruneStatus prune(Value value) throws PersistitException {
         byte[] valueBytes = value.getEncodedBytes();
         int valueSize = value.getEncodedSize();
 
-        if (!MVV.isArrayMVV(valueBytes, valueSize)) {
-            return true;
+        // TODO: Handle LONG_RECORD
+        if (isLongRecord(value) || !MVV.isArrayMVV(valueBytes, valueSize)) {
+            return PruneStatus.UNCHANGED;
         }
                 
         MVV.visitAllVersions(_pruneVisitor, valueBytes, valueSize);
-
-        int remaining = _pruneVisitor.getRemainingCount();
-        if (remaining == 0) {
-            // No valid versions left, key needs complete removed
-            return false;
+        if (_pruneVisitor.noVersionsLeft()) {
+            return PruneStatus.REMOVED;
         }
-
-        if (remaining == 1 && _pruneVisitor.getMaxVersion() != MVV.VERSION_NOT_FOUND) {
-            // Single committed version left, now primordial
+        if (_pruneVisitor.isPrimordial()) {
             int newSize = MVV.fetchVersionByOffset(valueBytes, valueSize, _pruneVisitor.getMaxOffset(), valueBytes);
             value.setEncodedSize(newSize);
-            return !(value.isDefined() && value.isAntiValue());
+            return (value.isDefined() && value.isAntiValue()) ? PruneStatus.REMOVED : PruneStatus.CHANGED;
         }
 
         // Some non-primordial amount left, remove all unneeded
         int newSize = valueSize;
         int offsetDiff = 0;
-        for(PruneVisitor.OffsetAndLength pair : _pruneVisitor.getRemoveList()) {
+        for (PruneVisitor.OffsetAndLength pair : _pruneVisitor.getRemoveList()) {
             int lengthToRemove = pair.length + MVV.LENGTH_PER_VERSION;
-            int dstPos = pair.offset- offsetDiff - MVV.LENGTH_PER_VERSION;
+            int dstPos = pair.offset - offsetDiff - MVV.LENGTH_PER_VERSION;
             int srcPos = dstPos + lengthToRemove;
             int copyLength = newSize - srcPos;
             System.arraycopy(valueBytes, srcPos, valueBytes, dstPos, copyLength);
@@ -3934,6 +3966,6 @@ public class Exchange {
             offsetDiff += lengthToRemove;
         }
         value.setEncodedSize(newSize);
-        return true;
+        return PruneStatus.CHANGED;
     }
 }
