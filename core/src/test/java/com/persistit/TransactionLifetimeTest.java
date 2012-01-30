c65ec7fb52a6a75d86261b6a97afde332c77ed40
Nathan Williams <nwilliams@akiban.com>
2012-01-30 18:06:01 -0500

    Fix WRITE_MANY handling and refactor a bit for clearer error messages

--- core/src/test/java/com/persistit/TransactionLifetimeTest.java
+++ core/src/test/java/com/persistit/TransactionLifetimeTest.java
@@ -16,6 +16,7 @@
 package com.persistit;
 
 import com.persistit.exception.PersistitException;
+import com.persistit.exception.TreeNotFoundException;
 import com.persistit.unit.PersistitUnitTestCase;
 
 import java.nio.ByteBuffer;
@@ -101,7 +102,7 @@ public class TransactionLifetimeTest extends PersistitUnitTestCase {
     }
 
     public void testBeginWriteManyAbort() throws PersistitException {
-        doTest(true, false, WRITE_MANY, ABORT);
+        doTest(true, true, WRITE_MANY, ABORT);
     }
 
     public void testBeginWriteManyCommit() throws PersistitException {
@@ -184,80 +185,154 @@ public class TransactionLifetimeTest extends PersistitUnitTestCase {
         doTest(false, false, WRITE_MANY, CHECKPOINT, COMMIT, RESTART);
     }
 
-
-    private static final int ABORT        = 1;
-    private static final int COMMIT       = 2;
-    private static final int WRITE        = 3;
-    private static final int WRITE_MANY   = 4;
-    private static final int CHECKPOINT   = 5;
-    private static final int RESTART      = 6;
-
+    
+    private static class Node {
+        public Node(String description) {
+            _description = description;
+        }
+        
+        @Override
+        public String toString() {
+            return _description;
+        }
+        
+        private final String _description;
+    }
+    
+    private static final Node ABORT        = new Node("ABORT");
+    private static final Node COMMIT       = new Node("COMMIT");
+    private static final Node WRITE        = new Node("WRITE");
+    private static final Node WRITE_MANY   = new Node("WRITE_MANY");
+    private static final Node CHECKPOINT   = new Node("CHECKPOINT");
+    private static final Node RESTART      = new Node("RESTART");
+    
 
     private static int storeFillTxnBuffer(Exchange ex, int writeCount) throws PersistitException {
-        ByteBuffer txnBuffer= ex.getTransaction().getTransactionBuffer();
-        final int remainingStart = txnBuffer.remaining();
-        while (txnBuffer.remaining() >= remainingStart) {
+        ByteBuffer txnBuffer = ex.getTransaction().getTransactionBuffer();
+        for (;;) {
+            final int prevPos = txnBuffer.position();
             ex.clear().append(KEY_PREFIX + writeCount);
             ex.getValue().clear().put(VALUE_PREFIX + writeCount);
             ex.store();
             ++writeCount;
+            if (prevPos > txnBuffer.position()) {
+                break;
+            }
         }
         return writeCount;
     }
+    
+    private void checkKeys(boolean shouldExist, int writeCount) throws PersistitException {
+        Exchange ex = null;
+        try {
+            ex = _persistit.getExchange(VOLUME_NAME, TREE_NAME, false);
+        } catch (TreeNotFoundException e) {
+            if (shouldExist && writeCount > 0) {
+                fail("Keys expected but tree does not exist: " + e);
+            } else {
+                return;
+            }
+        }
+        try {
+            for (int i = 0; i < writeCount; ++i) {
+                final String expectedKey = KEY_PREFIX + i;
+                boolean isDefined = ex.clear().append(expectedKey).isValueDefined();
+                assertEquals(expectedKey + " exists after test", shouldExist, isDefined);
+            }
+        } finally {
+            _persistit.releaseExchange(ex);
+        }
+    }
 
-    private void doTest(boolean expectedInIndex, boolean expectedInLiveMap, int... pieces) throws PersistitException {
-        final Exchange ex = _persistit.getExchange(VOLUME_NAME, TREE_NAME, true);
-        final Transaction txn = ex.getTransaction();
+    private void checkTransaction(String desc, long ts, Boolean statusExists, Boolean liveMapExists) {
+        if (statusExists != null) {
+            _persistit.getTransactionIndex().cleanup();
+            final TransactionStatus status = _persistit.getTransactionIndex().getStatus(ts);
+            final boolean actualInTxnIndex = status != null;
+            assertEquals("TransactionStatus exists after " + desc + "  ",
+                         statusExists.booleanValue(), actualInTxnIndex);
+        }
+
+        if (liveMapExists != null) {
+            final boolean actualInLiveMap = _persistit.getJournalManager().unitTestTxnExistsInLiveMap(ts);
+            assertEquals("Transaction in live map after " + desc + "  ",
+                         liveMapExists.booleanValue(), actualInLiveMap);
+        }
+    }
+
+    private void doTest(boolean expectedInIndex, boolean expectedInLiveMap, Node... nodes) throws PersistitException {
+        Exchange ex = _persistit.getExchange(VOLUME_NAME, TREE_NAME, true);
+        Transaction txn = ex.getTransaction();
+
         int writeCount = 0;
-
+        int fullWriteCount = 0;
+        boolean aborted = false;
+        boolean committed = false;
+        boolean currentInTxnIndex = true;
+        boolean currentInLiveMap = false;
+        String stateDescription = "BEGIN";
+        
         txn.begin();
         final long ts = txn.getStartTimestamp();
         try {
-            for (int piece : pieces) {
-                switch (piece) {
-                    case ABORT:
-                        txn.rollback();
-                    break;
-                    
-                    case COMMIT:
-                        txn.commit();
-                    break;
-                    
-                    case WRITE:
-                        ex.clear().append(KEY_PREFIX + writeCount);
-                        ex.getValue().clear().put(VALUE_PREFIX + writeCount);
-                        ex.store();
-                        ++writeCount;
-                    break;
-    
-                    case WRITE_MANY:
-                        writeCount = storeFillTxnBuffer(ex, writeCount);
-                    break;
-    
-                    case CHECKPOINT:
-                        CheckpointManager.Checkpoint cp = _persistit.checkpoint();
-                        assertEquals("Checkpoint successfully written", true, cp != null);
-                    break;
-
-                    case RESTART:
-                        safeCrashAndRestoreProperties();
-                        break;
-
-                    default:
-                        fail("Unknown test piece: " + piece);
-                    break;
+            for (Node curNode : nodes) {
+                stateDescription += "," + curNode;
+                
+                if (curNode == ABORT) {
+                    txn.rollback();
+                    aborted = true;
+                    currentInTxnIndex = (writeCount > 0);
+                    fullWriteCount += writeCount;
+                    writeCount = 0;
                 }
+                else if (curNode == COMMIT) {
+                    txn.commit();
+                    committed = true;
+                    currentInTxnIndex = false;
+                    currentInLiveMap = (writeCount > 0);
+                    fullWriteCount += writeCount;
+                    writeCount = 0;
+                }
+                else if (curNode == WRITE) {
+                    ex.clear().append(KEY_PREFIX + writeCount);
+                    ex.getValue().clear().put(VALUE_PREFIX + writeCount);
+                    ex.store();
+                    ++writeCount;
+                }
+                else if (curNode == WRITE_MANY) {
+                    writeCount = storeFillTxnBuffer(ex, writeCount);
+                    currentInLiveMap = true;
+                }
+                else if (curNode == CHECKPOINT) {
+                    CheckpointManager.Checkpoint cp = _persistit.checkpoint();
+                    assertEquals("Checkpoint successfully written", true, cp != null);
+                    currentInLiveMap = (!committed && writeCount > 0) || (aborted && writeCount == 0);
+                }
+                else if (curNode == RESTART) {
+                    txn = null;
+                    ex = null;
+                    currentInTxnIndex = false;
+                    currentInLiveMap = false;
+                    safeCrashAndRestoreProperties();
+                }
+                else {
+                    fail("Unknown test node: " + curNode);
+                }
+
+                checkTransaction(stateDescription, ts, currentInTxnIndex, currentInLiveMap);
             }
         } finally {
-            txn.end();
+            if (txn != null) {
+                txn.end();
+            }
         }
 
-        _persistit.getTransactionIndex().cleanup();
-        final TransactionStatus status = _persistit.getTransactionIndex().getStatus(ts);
-        final boolean actualInTxnIndex = status != null;
-        assertEquals("TransactionStatus exists after test", expectedInIndex, actualInTxnIndex);
+        checkTransaction("POST_CONDITION", ts, expectedInIndex, expectedInLiveMap);
 
-        final boolean actualInLiveMap = _persistit.getJournalManager().unitTestTxnExistsInLiveMap(ts);
-        assertEquals("Transaction exists in live map after test", expectedInLiveMap, actualInLiveMap);
+        if (aborted && committed) {
+            fail("Saw both commit AND abort?");
+        }
+
+        checkKeys(committed, fullWriteCount);
     }
 }
