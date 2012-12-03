f97d767c555f97ba56aef6b0a138de9e3593cddb
Peter Beaman <pbeaman@akiban.com>
2012-12-03 16:40:18 -0500

    Add missing license header

--- src/etc/prepare-maven-central.sh
+++ src/etc/prepare-maven-central.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 #
+# Copyright © 2012 Akiban Technologies, Inc.  All rights reserved.
+# 
+# This program and the accompanying materials are made available
+# under the terms of the Eclipse Public License v1.0 which
+# accompanies this distribution, and is available at
+# http://www.eclipse.org/legal/epl-v10.html
+# 
+# This program may also be available under different license terms.
+# For more information, see www.akiban.com or contact licensing@akiban.com.
+# 
+# Contributors:
+# Akiban Technologies, Inc.
+#
+# 
 # See https://docs.sonatype.org/display/Repository/Sonatype+OSS+Maven+Repository+Usage+Guide
 #
 # Run this script from the base directory.
